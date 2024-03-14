d2f9534e4eb957d607eca8ff3ccc8cdf992dc7bf
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-03-15 03:02:17 +0500

    Updated

---
+++ script.js
@@ -0,0 +1,15 @@
+// for (initializing ; condition ; increment / decrement){ }
+
+// var i;
+// for (i = 1 ; i <= 10 ; i++){
+//     console.log(i);
+// }
+
+// var i;
+// for (i = 10 ; i >= 0 ; i--){
+//     console.log(i);
+// }
+
+for (i = 2; i <= 40; i *= 2) {
+  console.log(i);
+}
