1660c4c305c96cde4778d9f817672da3c55118dc
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-03-17 23:39:56 +0500

    Updated

--- script.js
+++ script.js
@@ -10,6 +10,24 @@
 //     console.log(i);
 // }
 
-for (i = 2; i <= 40; i *= 2) {
-  console.log(i);
+// for (i = 2; i <= 40; i *= 2) {
+//   console.log(i);
+// }
+
+// for (var i = 1 ; i <= 10 ; i++){
+//   document.write( "2" + " " + "x" + " " + i + " " + "=" + " " + 2*i + "<br>")
+// }
+
+// var names = ['Ubaid ', 'Saqib ', 'Uzaima ', 'Adnan ', 'FAisal ', 'Kamran ', 'Usuf ', 'Taha '];
+// for (var i = 0 ; i<names.length ; i++){
+//   document.write(names[i]);
+// }
+
+var firstNames = ["Ubaid", "Saqib", "Uzaima"];
+var lastNames = ["Aslam", "Sheikh"];
+
+for (i = 0; i<firstNames.length; i++){
+  for (j = 0; j<lastNames.length; j++){
+    document.write(firstNames[i] + " " + lastNames[j] + "<br>")
+  }
 }
